d64364a5bd22d781dea4b4deeec17af5c474a9df
Florian Walther <flw@scusi.io>
2026-01-14 22:35:56 +0100

    added fingerprint option to spipeKeygen

--- cmd/spipeKeygen/spipeKeygen.go
+++ cmd/spipeKeygen/spipeKeygen.go
@@ -6,6 +6,7 @@ package main
 import (
 	"encoding/base64"
 	"crypto/rand"
+        "crypto/sha256"
 	"io/ioutil"
 	"flag"
 	"fmt"
@@ -13,10 +14,12 @@ import (
 )
 
 var base64Out bool
+var fingerprint bool
 var outfile string
 
 func init() {
 	flag.BoolVar(&base64Out, "base64out", false, "if true key will be printed as base64 value and not written to file")
+	flag.BoolVar(&fingerprint, "fingerprint", true, "if true fingerprint of key will be printed. Default: true")
 	flag.StringVar(&outfile, "o", "spipe.key", "file output will be written to, default 'spipe.key'.")
 }
 
@@ -27,6 +30,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	if fingerprint {
+            	h := sha256.New()
+		h.Write(randbuf)
+		s := h.Sum(nil)
+		encodedStr := base64.StdEncoding.EncodeToString(s[:32])
+		fmt.Printf("Key fingerprint (base64): %s\n", encodedStr)
+		fmt.Printf("Key fingerprint (hex)   : %x\n", s[:32])
+        }
 	if base64Out {
 		encodedStr := base64.StdEncoding.EncodeToString(randbuf)
 		fmt.Printf("%s\n", encodedStr)
